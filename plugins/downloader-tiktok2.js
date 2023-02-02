450845a0938144cfb9fefa69a3e5182c82f252a5
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2023-02-03 00:52:18 +0700

    Update downloader-tiktok2.js

--- plugins/downloader-tiktok2.js
+++ plugins/downloader-tiktok2.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, command, text }) => {
   if (!text) return m.reply(`Use Example: ${usedPrefix+ command} https://vt.tiktok.com/ZSJE2ffo4`)
-  let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=${global.lolkey}&url=${text}`)
+  let res = await fetch(`https://api.zahwazein.xyz/downloader/tiktok?apikey=79ec223a85&url=${text}`)
   let res2 = await res.json()
   let x = res2.result
   let cap = `Nih ${conn.getName(m.sender)}`
